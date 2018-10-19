919f7e5867f0fa22d199d531bbbc8c861907d947
Italo Cocchini <italococchini@gmail.com>
2018-10-19 10:21:56 -0430

    prueba

---
+++ peru_suspension/pagos/reporte_pagos.js
@@ -0,0 +1,262 @@
+var table = ""; var CTX = "";
+var fechas = '';
+
+// Variables por defecto de busqueda
+var _hiddenDefault = { "nuevo":[1,2,9,11], 'generados': [0] };
+var _tipo = 'nuevo';
+var _hiddenColumns = _hiddenDefault.nuevo;
+
+jQuery(document).ready(function() {
+
+	loadTabla( _tipo, _hiddenColumns );
+
+    jQuery("#close_modal").on("click", function(e){
+        cerrar(e);
+    });
+ 
+    jQuery("#form-search").on("submit", function(e){
+		e.preventDefault();
+    });
+
+    jQuery("#btn-search").on("click", function(e){
+		loadTabla( _tipo, _hiddenColumns );
+	});
+
+    jQuery(document).on('click', "[data-modal='comentarios']", function(){
+		abrir_link( jQuery(this) );
+    });
+
+    jQuery('[data-action="popover"] li').on('click', function(e){
+    	var content_old = jQuery( '#popover-content > span' ).html();
+    	var content = jQuery(this).attr('data-content');
+    	jQuery('[data-action="popover"] li a div').css('background-color', '#ccc'); 	    	
+    	if( content != content_old ){		
+	    	jQuery( '#popover-content' ).css( 'visibility', 'visible' );
+	    	jQuery( '#popover-content > span' ).html( content );
+	    	jQuery(this).children('a').children('div').css('background-color', '#22712c'); 	    	
+    	}else{
+	    	jQuery( '#popover-content' ).css( 'visibility', 'hidden' );
+	    	jQuery( '#popover-content > span' ).html( '' );
+    	}
+    });
+
+    jQuery(document).on('click', "[data-action='error']", function(e){
+    	e.preventDefault();
+    	alert( jQuery(this).attr('title') );
+    	return false;
+    });
+
+    jQuery("#select-all").on("click", function(e){
+    	if( jQuery("input[data-type='item_selected']:checked").size() > 0 ){
+			jQuery("input[data-type='item_selected']").prop('checked', '' );
+    	}else{
+			jQuery("input[data-type='item_selected']").prop('checked', 'checked' );
+    	}
+		updateTotalTag();
+    });
+
+    jQuery(document).on('click', '[data-type="item_selected"]' ,function(e){
+    	updateTotalTag();
+    });
+
+	jQuery('.nav-link').on('click', function (e) {
+		e.preventDefault();
+		jQuery('.nav-link').removeClass('active');
+
+	  	_tipo = jQuery(this).attr('href'); 
+
+		var id = jQuery(this).attr('id');
+		jQuery('#'+id).addClass('active');
+		jQuery('#'+id+'-tab').addClass('active');
+
+		_hiddenColumns = _hiddenDefault.nuevo;
+		jQuery('[name="ini"]').val(fecha.ini);
+		jQuery('[name="fin"]').val(fecha.fin);
+		jQuery('#opciones-nuevo').css('display', 'block');
+
+		if( jQuery('#'+id).attr('href') == 'generados' || jQuery('#'+id).attr('href') == 'completado'){
+			jQuery('#opciones-nuevo').css('display', 'none');
+			jQuery('[name="ini"]').val("YYYY-MM-DD");
+			jQuery('[name="fin"]').val("YYYY-MM-DD");
+
+			jQuery('[name="fin"]').removeAttr('min');
+			jQuery('[name="fin"]').removeAttr('max');
+			_hiddenColumns = _hiddenDefault.generados;
+		}
+  		 
+	  	loadTabla( _tipo, _hiddenColumns );
+		updateTotalTag();
+	});
+
+	jQuery('#quitar-filtro').on('click', function(){
+		jQuery('[name="ini"]').val("YYYY-MM-DD");
+		jQuery('[name="fin"]').val("YYYY-MM-DD");
+		loadTabla( _tipo, _hiddenColumns );
+	});
+ 
+    jQuery(document).on('click', '[data-action="procesar"]', function(e){
+    	e.preventDefault();
+    	if( jQuery(this).attr('data-target') == 'autorizado' ){		
+	    	var users = [];
+			jQuery.each(jQuery("input:checked"), function(){
+				var user = jQuery(this).val();
+				var token = jQuery(this).attr('data-token');	
+				users.push({'token':token, 'user_id':user});
+			});
+			generar_solicitud( users, jQuery(this).attr('data-target') );
+    	}else{
+    		cerrar();
+    	}
+	});
+	  
+    jQuery("[data-modal='autorizar']").on('click', function(){
+    	if( jQuery("input:checked").size() > 0 ){
+    		abrir_link( jQuery(this) );
+    	}else{
+    		alert('Debe seleccionar los registros a procesar');
+    	}
+    });  
+
+	jQuery('[name="ini"]').on('change', function(){
+		console.log(_tipo);
+
+		jQuery('[name="fin"]').removeAttr('min');
+		jQuery('[name="fin"]').removeAttr('max');
+		if( _tipo == 'nuevo' ){		
+			var d = new Date( jQuery(this).val() );
+			var limitDate = sumarDias(d, 30);
+
+			jQuery('[name="fin"]').val( limitDate );
+			jQuery('[name="fin"]').attr('min', jQuery(this).val() );
+			jQuery('[name="fin"]').attr('max', limitDate );
+		}
+	});
+
+});
+
+function sumarDias(fecha, dias){
+	fecha.setDate(fecha.getDate() + dias);
+	var d = '0'+fecha.getDate();
+		d = d.substring(d.length-2, d.length);
+	var m = fecha.getMonth();
+		m += 1;
+		m = '0'+m;
+		m = m.substring(m.length-2, m.length);
+	var y = fecha.getFullYear();
+ 
+	return y+'-'+m+'-'+d;
+}
+
+function updateTotalTag(){
+	var total = 0;
+    jQuery.each( jQuery("input[data-type='item_selected']:checked"), function(i,v){
+    	total += parseFloat(jQuery(this).attr('data-total'));
+    });
+	jQuery('#pagosNuevos-tab span').html('$ '+total);
+}
+
+function total_pagos_generados(){
+	jQuery.post(
+		TEMA+'/admin/backend/pagos/ajax/total_pagos_procesados.php',
+		{ "tipo": _tipo, 'desde': jQuery('[name="ini"]').val(), "hasta":jQuery('[name="fin"]').val() },
+		function(data){
+			data = JSON.parse(data);
+			jQuery('#pagosGenerados-tab span').html('$ '+data['total']);
+		}
+	);
+}
+
+function generar_solicitud( users, accion ){
+	jQuery.post(
+		TEMA+'/admin/backend/pagos/ajax/generar_solicitud.php',
+		{
+			'ID':ID, 
+			'users':users,
+			'accion':accion, 
+			'comentario': jQuery('[name="observaciones"]').val()
+		},
+		function(data){
+			loadTabla( _tipo, _hiddenColumns );	
+			cerrar();
+		}
+	);
+}
+
+function loadTabla( tipo, hiddenColumns ){
+ 	 
+	table = jQuery('#example').DataTable();
+	table.destroy();
+
+    table = jQuery('#example').DataTable({
+    	"language": {
+			"emptyTable":			"No hay datos disponibles en la tabla.",
+			"info":		   			"Del _START_ al _END_ de _TOTAL_ ",
+			"infoEmpty":			"Mostrando 0 registros de un total de 0.",
+			"infoFiltered":			"(filtrados de un total de _MAX_ registros)",
+			"infoPostFix":			" (actualizados)",
+			"lengthMenu":			"Mostrar _MENU_ registros",
+			"loadingRecords":		"Cargando...",
+			"processing":			"Procesando...",
+			"search":				"Buscar:",
+			"searchPlaceholder":	"Dato para buscar",
+			"zeroRecords":			"No se han encontrado coincidencias.",
+			"paginate": {
+				"first":			"Primera",
+				"last":				"Última",
+				"next":				"Siguiente",
+				"previous":			"Anterior"
+			},
+			"aria": {
+				"sortAscending":	"Ordenación ascendente",
+				"sortDescending":	"Ordenación descendente"
+			}
+		},
+		"dom": '<B><f><t>ip',
+		"buttons": [
+			{
+			  extend: "csv",
+			  className: "btn-sm"
+			},
+			{
+			  extend: "excelHtml5",
+			  className: "btn-sm"
+			},
+        ],
+		columnDefs: [
+            {
+            	"targets": hiddenColumns,
+                "visible": false
+            }
+        ],
+        "scrollX": true,
+        "ajax": {
+            "url": TEMA+'/admin/backend/pagos/ajax/pagos.php',
+            "data": { "tipo": _tipo, 'desde': jQuery('[name="ini"]').val(), "hasta":jQuery('[name="fin"]').val() },
+            "type": "POST"
+        }
+	});
+
+	total_pagos_generados();
+}
+
+function abrir_link(e){
+	init_modal({
+		"titulo": e.attr("data-titulo"),
+		"modulo": "pagos",
+		"modal": e.attr("data-modal"),
+		"info": {
+			"ID": e.attr("data-id")
+		}
+	});
+}
+
+ 
+
+
+
+
+
+
+
+
+ 
---
+++ peru_suspension/pagos/reporte_pagos.php
@@ -0,0 +1,152 @@
+<pre>
+<?php 
+    include_once('lib/pagos.php'); 
+    $fecha = $pagos->getRangoFechas();
+    $user = wp_get_current_user();
+?>
+</pre>
+<script>
+    var ID = <?php echo $user->ID; ?>;
+    var fecha = { 
+        ini:'<?php echo $fecha['ini']; ?>', 
+        fin:'<?php echo $fecha['fin']; ?>'
+    };    
+</script>
+<link rel='stylesheet' type='text/css' href='<?php echo getTema() ?>/admin/backend/pagos/reporte_pagos.css'>
+<script src='<?php echo getTema(); ?>/admin/backend/pagos/reporte_pagos.js'></script>
+
+<div class="container_listados">
+
+    <div class='titulos'>
+        <h2>Control de Pagos a cuidadores</h2>
+        <hr>
+    </div>
+
+    <div class='col-md-12'>
+        <div class='row'>
+            <div class="col-sm-12 col-md-5">
+                <button class="btn btn-defaut" id="select-all"><i class="fa fa-list"></i> Marcar/Desmarcar Todos</button>
+                <button class="btn btn-defaut" id="quitar-filtro"><i class="fa fa-filter"></i> Mostrar todos</button>
+            </div>
+            <div class="col-sm-12 col-md-7 container-search text-right">
+                <form id="form-search" name="search">
+                    <span><label class="fecha">Desde: </label><input type="date" name="ini" value="<?php echo $fecha['ini']; ?>"></span>
+                    <span><label class="fecha">Hasta: <input type="date" name="fin" 
+                        min="<?php echo $fecha['min']; ?>" 
+                        max="<?php echo $fecha['max']; ?>" 
+                        value="<?php echo $fecha['fin']; ?>"></label></span> 
+                    <button class="btn btn-defaut" id="btn-search"><i class="fa fa-search"></i> Buscar</button>
+                </form>
+            </div>
+        </div>
+        <hr>
+    </div>
+    <div class="clear"></div>
+
+    <div class='col-md-12'>
+
+        <dir class="leyenda text-right">
+            <div class="col-md-12">
+                
+                <ul data-action="popover" class="list-inline">
+                    <li><strong>LEYENDA DET. RESERVAS: </strong></li>
+                    <li data-content="<strong>Normal: </strong> No posee descuento">
+                        <a href="javascript:;">
+                            <aside style="background:#a6a5a5;"></aside> Normal
+                        </a>
+                    </li>
+                    <li data-content="<strong>Cupón: </strong> Un Cupón fue aplicado">
+                        <a href="javascript:;">
+                            <aside style="background:#8d88e0;"></aside> Cupón 
+                        </a>
+                    </li>
+                    <li data-content="<strong>Saldo a favor: </strong> El Saldo a favor fue aplicado.">
+                        <a href="javascript:;">
+                            <aside style="background:#88e093;"></aside> Saldo a favor
+                        </a>
+                    </li>
+                    <li data-content="<strong>Ambos: </strong> Se aplico el Saldo a favor y un Cupón de Descuento">
+                        <a href="javascript:;">
+                            <aside style="background:#e0888c;"></aside> Ambos  
+                        </a>
+                    </li>
+                </ul>
+                
+                <ul data-action="popover" class="list-inline">
+                    <li><strong>LEYENDA DE ESTATUS: </strong></li>
+                    <li data-content="<strong>Por autorizar: </strong> El supervisor debe autorizar la solicitud">
+                        <a href="javascript:;">
+                            <div></div> Por autorizar
+                        </a>
+                    </li>
+                    <li data-content="<strong>Autorizado: </strong> La solicitud esta autorizada y el pago no fue procesado">
+                        <a href="javascript:;">
+                            <div></div> Autorizado
+                        </a>
+                    </li>
+                    <!-- li data-content="<strong>Negado: </strong> La solicitud fue negada por los supervisores"><a href="javascript:;"><div></div> Negado</a></li -->
+                    <li data-content="<strong>En progreso: </strong> La solicitud esta en proceso de pago por la entidad bancaria"><a href="javascript:;"><div></div> En progreso</a></li>
+                    <li data-content="<strong>Cancelado: </strong> La solicitud de pago fue cancelada"><a href="javascript:;"><div></div> Cancelado</a></li>
+                    <li data-content="<strong>Completado: </strong> La solicitud de pago fue procesada"><a href="javascript:;"><div></div> Completado</a></li>
+                    <li data-content="<strong>Error: </strong> Ocurrio un error al procesar la solicitud de pago"><a href="javascript:;"><div></div> Error</a></li>
+                </ul>
+
+            </div>
+
+            <div>
+                <div id="popover-content" class="pull-right text-left alert alert-info">
+                    <i style="margin-right:5px;padding:5px 15px 5px 0px;border-right: 1px solid #ccc;" class="fa fa-info-circle" aria-hidden="true"></i> 
+                    <span></span>
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close" style="margin-left: 20px;">
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="clear"></div>
+            </div>  
+        </dir>
+        <div>
+            <ul class="nav nav-tabs" id="myTab" role="tablist">
+              <li class="nav-item">
+                <a class="nav-link active" id="pagosNuevos-tab" data-toggle="tab" href="nuevo" role="tab" aria-controls="pagosNuevos" aria-selected="true">Pendientes de pago <span class="badge-total">$ 0</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" id="pagosGenerados-tab" data-toggle="tab" href="generados" role="tab" aria-controls="pagosGenerados" aria-selected="false">Enviadas a pago <span class="badge-total">$ 0</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" id="pagosCompletado-tab" data-toggle="tab" href="completado" role="tab" aria-controls="pagosCompletado" aria-selected="false">Pago completado </a>
+              </li>
+            </ul>
+
+            <div class="botones_container" id="opciones-nuevo">
+                <button class="btn btn-success" data-titulo='Procesar solicitudes de pago' data-modal='autorizar' data-id="0" ><i class="fa fa-money"></i> Generar Solicitud de pago</button>
+            </div>
+            
+            <table id="example" class="table table-striped table-bordered nowrap" cellspacing="0" style="min-width: 100%;">
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th>Fecha</th>
+                        <th>Estatus</th>
+                        <th>ID</th>
+                        <th>Nombre</th>
+                        <th>Apellido</th>
+                        <th>Total a pagar</th>
+                        <th>Cant. Reservas</th>
+                        <th>Det. Reservas</th>
+                        <th>Autorizado por.</th>
+                        <th>Opciones</th>
+                        <th>Observaciones</th>
+                    </tr>
+                </thead>
+                <tbody></tbody>
+            </table>
+        </div>
+    </div>
+
+
+</div>
+
+ <?php
+    include_once(dirname(dirname(__DIR__)).'/recursos/modal.php');
+?>
+ 
