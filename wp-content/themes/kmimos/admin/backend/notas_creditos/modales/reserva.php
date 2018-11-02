70879942a49289cf1f2f2b7867d1c87120df3d55
Italo Cocchini <italococchini@gmail.com>
2018-11-02 10:45:19 -0400

    act

--- wp-content/themes/kmimos/admin/backend/notas_creditos/modales/reserva.php
+++ wp-content/themes/kmimos/admin/backend/notas_creditos/modales/reserva.php
@@ -10,8 +10,8 @@
 
 
 	$reserva_end = get_post_meta( $ID, '_booking_end', true );
-	if( $pedido_id > 0 && date('Y-m-d') >= date('Y-m-d', strtotime($reserva_end)) ){
-		//$pedido_id = 0;
+	if( $pedido_id > 0 && date('Y-m-d') < date('Y-m-d', strtotime($reserva_end)) ){
+		$pedido_id = 0;
  	}
 
 
@@ -49,7 +49,7 @@
 </script> 
 
 <?php if( !$show_nc ){ ?>
-	<input type="text" value="<?php print_r($pedido_id.' > 0 && '. date('Y-m-d') .'<='. date('Y-m-d', strtotime($reserva_end)))?>">
+	<input type="text" value="<?php print_r(date('Y-m-d', strtotime($reserva_end)))?>">
 	<div class="text-center" style="display: <?php echo $show_msg; ?>">
 		<p style=" font-weight: bold; padding: 20px 0px 0px 0px;">
 			<?php echo $msg; ?>
