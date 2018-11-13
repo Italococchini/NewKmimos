2f48e4b668fa4b21ffcd61568864dfca29bda7de
Italo Cocchini <italococchini@gmail.com>
2018-11-13 14:17:48 -0400

    dev test

--- wp-content/themes/kmimos/procesos/reservar/emails/index.php
+++ wp-content/themes/kmimos/procesos/reservar/emails/index.php
@@ -348,8 +348,8 @@
 
 			if( $acc == "CFM" ){
 
-				//$wpdb->query("UPDATE wp_posts SET post_status = 'wc-confirmed' WHERE ID = '{$servicio["id_orden"]}';");
-	    		//$wpdb->query("UPDATE wp_posts SET post_status = 'confirmed' WHERE ID = '{$servicio["id_reserva"]}';");
+				$wpdb->query("UPDATE wp_posts SET post_status = 'wc-confirmed' WHERE ID = '{$servicio["id_orden"]}';");
+	    		$wpdb->query("UPDATE wp_posts SET post_status = 'confirmed' WHERE ID = '{$servicio["id_reserva"]}';");
 
 				include("confirmacion.php");
 
