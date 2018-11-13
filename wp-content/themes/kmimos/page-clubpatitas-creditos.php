1f864b7d368cb463f88919e483c16a3c2a585d18
Italo Cocchini <italococchini@gmail.com>
2018-11-13 19:59:54 -0400

    ajustes

--- wp-content/themes/kmimos/page-clubpatitas-creditos.php
+++ wp-content/themes/kmimos/page-clubpatitas-creditos.php
@@ -42,7 +42,7 @@
 		</aside>
 		<section class="col-xs-12 col-sm-12 col-md-7 compartir-section" style="<?php echo $center_content; ?>">
 			
-			<dir class="col-md-8 col-md-offset-2">
+			<dir class="col-md-12 ">
 				
 				<div class="row">
 					<img src="<?php echo getTema().'/images/club-patitas/Kmimos-Club-de-las-patitas-felices-6.png'; ?>">
