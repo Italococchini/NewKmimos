004b2da844bdfde41a5fdd40db5c4324829fff8b
Italo Cocchini <italococchini@gmail.com>
2018-11-13 16:18:54 -0400

    ajustes

--- wp-content/themes/kmimos/procesos/clubPatitasFelices/ajax/creditos.php
+++ wp-content/themes/kmimos/procesos/clubPatitasFelices/ajax/creditos.php
@@ -3,22 +3,26 @@
 	session_start();
 	include ( '../../../../../../wp-load.php' );
 
-	$creditos = $wpdb->get_results("select * from cuidadores_transacciones where tipo='saldo_club' and user_id = ".$user_id);
-
     $data = array(
         "data" => array()
     );
 
-	if( !empty($creditos) ){
-		$count=0;
-		foreach ($creditos as $row) {
-			$count++;
-			$data["data"][] = array(
-				$count,
-                date('Y-m-d',strtotime($row->fecha)),
-                utf8_encode($row->descripcion),
-                $row->monto
-            );
+	$user = wp_get_current_user();
+	if( isset($user->ID) ){	
+		$user_id = $user->ID;
+
+		$creditos = $wpdb->get_results("select * from cuidadores_transacciones where tipo='saldo_club' and user_id = ".$user_id);
+		if( !empty($creditos) ){
+			$count=0;
+			foreach ($creditos as $row) {
+				$count++;
+				$data["data"][] = array(
+					$count,
+	                date('Y-m-d',strtotime($row->fecha)),
+	                utf8_encode($row->descripcion),
+	                $row->monto
+	            );
+			}
 		}
 	}
 
