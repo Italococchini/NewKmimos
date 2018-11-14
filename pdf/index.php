301ee8a63a82f15a41471f76626778ff7d5615fd
Italo Cocchini <italococchini@gmail.com>
2018-11-14 13:14:11 -0400

    ajustes

--- pdf/index.php
+++ pdf/index.php
@@ -10,6 +10,9 @@
     $url_img = get_home_url() .'/wp-content/themes/kmimos/images/club-patitas/';
     $no_top_menu = false;
 
+	wp_enqueue_style( 'bootstrap.min', getTema()."/css/bootstrap.min.css", array(), "1.0.0" );
+	wp_enqueue_style( 'kmimos_style', getTema()."/css/kmimos_style.css", array(), "1.0.0" );
+
     wp_enqueue_style('club_style', getTema()."/css/club-patitas-felices.css", array(), '1.0.0');
     wp_enqueue_style('club_responsive', getTema()."/css/responsive/club-patitas-felices.css", array(), '1.0.0');
 	wp_enqueue_script('club_script', getTema()."/js/club-patitas-felices.js", array(), '1.0.0');
