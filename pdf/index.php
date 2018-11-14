60811990c765a6d688dd0f92d8a606c78595dcee
Italo Cocchini <italococchini@gmail.com>
2018-11-14 13:50:55 -0400

    ajustes

--- pdf/index.php
+++ pdf/index.php
@@ -3,59 +3,55 @@
 	require dirname(__DIR__).'/wp-load.php';
 	use Spipu\Html2Pdf\Html2Pdf;
 
-
 	$user = wp_get_current_user();
 	$cupon = get_user_meta( $user->ID, 'club-patitas-cupon', true );
-	
-    $url_img = get_home_url() .'/wp-content/themes/kmimos/images/club-patitas/';
-    $no_top_menu = false;
-
-	wp_enqueue_style( 'bootstrap.min', getTema()."/css/bootstrap.min.css", array(), "1.0.0" );
-	wp_enqueue_style( 'kmimos_style', getTema()."/css/kmimos_style.css", array(), "1.0.0" );
-
-    wp_enqueue_style('club_style', getTema()."/css/club-patitas-felices.css", array(), '1.0.0');
-    wp_enqueue_style('club_responsive', getTema()."/css/responsive/club-patitas-felices.css", array(), '1.0.0');
-	wp_enqueue_script('club_script', getTema()."/js/club-patitas-felices.js", array(), '1.0.0');
-	ob_start();
 ?>
-	<div class="content-compartir-club">
-		<div id="compartir-club-cover" class="col-xs-12 col-sm-12 col-md-5" style="background-image: url(<?php echo getTema();?>/images/club-patitas/Kmimos-Club-de-las-patitas-felices-1.jpg);">
-		</div>
-		<div class="col-xs-12 col-sm-12 col-md-7 compartir-section" style="<?php echo $center_content; ?>">
-			
-			<dir class="col-md-8 col-md-offset-2">
-				
-				<div class="row">
-					<img src="<?php echo getTema().'/images/club-patitas/Kmimos-Club-de-las-patitas-felices-6.png'; ?>">
-					<h2 class="titulo">¡Ya eres parte del club!</h2>
-					<p style="
-						font-weight: bold; 
-						font-size: 18px; 
-						text-align: center;
-						margin-top: 10%;
-						">Tu código único del club</p>
+	<!DOCTYPE html>
+	<html>
+		<head>
+			<meta charset="UTF-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+			<link rel="stylesheet" type="text/css" media="all" href="<?php echo getTema(); ?>/css/bootstrap.min.css"></link>
+		    <link rel="stylesheet" type="text/css" media="all" href="<?php echo getTema(); ?>/css/club-patitas-felices.css"></link>
 
-					<div class="cupon">
-						<?php echo strtoupper($cupon); ?>
-					</div>
+		</head>
+		<body>
+			<div class="content-compartir-club">
+				<div id="compartir-club-cover" class="col-xs-12 col-sm-12 col-md-5" style="
+					background-image: url(<?php echo getTema();?>/images/club-patitas/Kmimos-Club-de-las-patitas-felices-1.jpg);
+				"></div>
+				<div class="col-xs-12 col-sm-12 col-md-7 compartir-section" style="<?php echo $center_content; ?>">
+					
+					<dir class="col-md-8 col-md-offset-2 text-center">
+						
+						<div class="row">
+							<img src="<?php echo getTema().'/images/club-patitas/Kmimos-Club-de-las-patitas-felices-6.png'; ?>">
+							<h2 class="titulo">¡Ya eres parte del club!</h2>
+							<p style="
+								font-weight: bold; 
+								font-size: 18px; 
+								text-align: center;
+								margin-top: 10%;
+								">Tu código único del club</p>
 
-					<p style="font-weight: bold; font-size: 16px;">Hemos enviado tu código a la cuenta de correo regístrada</p>
-					<p>Recuerda, por cada vez que alguien use tu código y complete una reservación con un Cuidador
-					Kmimos <strong>tú ganas $150 MXN</strong> acumulables.</p>
-					<p style="font-weight:bold;font-size:16px;color:#0D7AD8;">¡Más compartes, más ganas! </p>
+							<div class="cupon">
+								<?php echo strtoupper($cupon); ?>
+							</div>
+
+							<p style="font-weight: bold; font-size: 16px;">Hemos enviado tu código a la cuenta de correo regístrada</p>
+							<p>Recuerda, por cada vez que alguien use tu código y complete una reservación con un Cuidador
+							Kmimos <strong>tú ganas $150 MXN</strong> acumulables.</p>
+							<p style="font-weight:bold;font-size:16px;color:#0D7AD8;">¡Más compartes, más ganas! </p>
+
+						</div>
+
+					</dir>
 
 				</div>
-
-			</dir>
-
-		</div>
-	</div>
+			</div>
+		</body>
+	</html>
 <?php
-	$html = ob_get_contents();
-	ob_end_clean();
-
-echo $html;
-
 	// $html2pdf = new Html2Pdf();
 	// $html2pdf->writeHTML( $html );
 	// $html2pdf->output();
